c902e26f6b45971f6ae920ab0ee79fc594a7be91
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-07-30 22:09:16 -0400

    Implemented the homepage for the GET news route. Added a temp solution to not use sessions

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,11 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.user_id) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
+  next();
+  // console.log(req.session)
+  //   if (!req.session.user_id) {
+  //     res.redirect('/login');
+  //   } else {
+  //     next();
+  //   }
 };
 
 module.exports = withAuth;
