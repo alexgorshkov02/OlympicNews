e5e34f1f5b2f2f0afd89317d7d6425759eb5126b
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-08-01 16:59:13 -0400

    Fixed the issue when the app get a null fields from newsApi. Fixed the routes. Moved the singup part to a separate page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,4 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Comment, News, User} = require('../models');
 
 router.get('/', (req, res) => {
     res.render('dashboard', { loggedIn: true });
