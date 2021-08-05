96ab55ec2c6630e441169731d5d0d3a1e5be5618
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-08-04 23:39:00 -0400

    Implemented displaying the news and comments for them. Implemented a custom handlebar helper. Added edit and remove button for the comments

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-
-const path = require('path');
+const path = require("path");
 const express = require("express");
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ extname: ".handlebars" });
 const sequelize = require("./config/connection");
-const session = require('express-session');
+const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const Handlebars = require("handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+Handlebars.registerHelper("list", function (context) {
+  let result = "<ul>";
+
+  for (let prop in context) {
+    // Get the title to display
+    const title = "<li>" + prop;
+
+    // Get the comments for the title to display
+    const comments = context[prop].map((el) => {
+      return `<li> ${el.comment} <button type="button" id="edit-${el.commentID}">Edit</button> <button type="button" id="remove-${el.commentID}">Remove</button></li>`;
+    });
+
+    // Merge the title and the comments
+    result += `${title} <ul> ${comments.join(" ")} </ul> </li> </br> </br>`;
+  }
+  return result + "</ul>";
+});
 
 const sess = {
   secret: "Super secret word",
@@ -22,17 +40,17 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(require("./controllers/"));
 
-
-app.get('/', function (req, res) {
-  res.render('homepage');
+app.get("/", function (req, res) {
+  res.render("homepage");
 });
 
 // turn on connection to db and server
