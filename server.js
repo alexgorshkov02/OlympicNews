c2bebb434452d00a098a08940ccb4199f82b6886
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-08-14 20:00:43 -0400

    To create a new database on heroku. Test2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-// const hbs = exphbs.create({ extname: ".handlebars" });
+// const hbs = exphbs.create({ extname: ".handlebars" }); 
 const sequelize = require("./config/connection");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
